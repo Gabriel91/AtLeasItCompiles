e95ec336f4f61c7eb767e7e43881f39063d788f5
Gabriel91 <gdpfhc@mail.missouri.edu>
2015-07-19 00:12:20 -0500

    STRING BUFFEREDREADER SAVED MY LIFE

--- DateTime/src/JSONGenerator.java
+++ DateTime/src/JSONGenerator.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class JSONGenerator {
@@ -5,9 +6,15 @@ public class JSONGenerator {
 	public static void main(String[] args) {
 		ArrayList<String> twitList = new ArrayList<String>();
 		TweetToquenizer toquenizer = new TweetToquenizer();
-		twitList = toquenizer.readFileIntoArrayList();
+//		twitList = 
+		try {
+			toquenizer.readFileIntoArrayList();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		System.out.println(twitList.toString());
+//		System.out.println(twitList.toString());
 	}
 
 }
--- DateTime/src/TweetToquenizer.java
+++ DateTime/src/TweetToquenizer.java
@@ -1,23 +1,41 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.Normalizer;
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class TweetToquenizer {
 
-	public ArrayList<String> readFileIntoArrayList() {
-		Scanner scanner = null;
+	public void readFileIntoArrayList() throws IOException {
+		FileReader twitterFile = null;
+		String line = null;
 		try {
-			scanner = new Scanner(new File(
-					"..//DateTime/src/TwitterData/BasicTwitter.txt"));
+			twitterFile = new FileReader(
+					"..//DateTime/src/TwitterData/BasicTwitter.txt");
+			BufferedReader br = new BufferedReader(twitterFile);
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+			twitterFile.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		ArrayList<String> list = new ArrayList<String>();
-		while (scanner.hasNextLine()) {
-			list.add(scanner.nextLine());
-		}
-		scanner.close();
-		return list;
 	}
+	/*
+	 * try { scanner = new Scanner(twitterFile); } catch (FileNotFoundException
+	 * e) { // TODO Auto-generated catch block e.printStackTrace(); }
+	 * ArrayList<String> list = new ArrayList<String>(); while
+	 * (scanner.hasNextLine()) { System.out.println(scanner.hasNext()); String
+	 * english = deAccent(scanner.nextLine()); System.out.println(english);
+	 * list.add(english); } scanner.close(); return list; }
+	 * 
+	 * public String deAccent(String str) { String nfdNormalizedString =
+	 * Normalizer.normalize(str, Normalizer.Form.NFD); Pattern pattern =
+	 * Pattern.compile("\\p{InCombiningDiacriticalMarks}+"); return
+	 * pattern.matcher(nfdNormalizedString).replaceAll(""); }
+	 */
 }
