eade9ec5aa1816b4fada92776f58d1d65b05fee6
Gabriel91 <gdpfhc@mail.missouri.edu>
2015-07-19 05:45:26 -0500

    Galan changes

--- DateTime/src/SubjectSearch.java
+++ DateTime/src/SubjectSearch.java
@@ -2,13 +2,25 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class SubjectSearch {
 	final static String POSITIVE = "positive\t";
 	final static String NEGATIVE = "negative\t";
 	final static String NEUTRAL = "neutral\"";
+	
+	public static void main(String[] args) throws IOException
+	{
+		String inputString = "";
+		for (int i = 0; i < args.length;i++)
+		{
+			inputString += args[i];
+		}
+		keyWordSearch(inputString);
+	}
 
-	public static void main(String[] args) throws IOException {
+	public static String keyWordSearch(String subject) throws IOException {
+		subject = subject.trim().toLowerCase();
 		int positives = 0;
 		int negatives = 0;
 		int neutrals = 0;
@@ -16,12 +28,11 @@ public class SubjectSearch {
 		FileReader analyzedTweets = new FileReader(
 				"..//DateTime/src/TwitterData/TweetsAndAnalysis.txt");
 		String line = null;
-		String subject = "soda";//args[0].toLowerCase();
 
 		BufferedReader br = new BufferedReader(analyzedTweets);
 		int match = 0;
 		while ((line = br.readLine()) != null) {
-			line = line.toLowerCase();
+			line = line.trim().toLowerCase();
 			int index = -1;
 			if ((index = line.indexOf(subject)) != -1) {
 				match++;
@@ -44,6 +55,8 @@ public class SubjectSearch {
 		}
 		analyzedTweets.close();
 		System.out.println("positives: " + positives + "\nnegatives: " + negatives + "\nneutral: " + neutrals + "   match: " + match);
+		String results = positives + "," + negatives + "," + neutrals;
+		return results;
 	}
 
 }
